029ab2b2cefa40d094a562103f1157e4a7650e1c
James Kua <kua.james30@gmail.com>
2024-05-07 10:17:11 +0800

    Add SearchInput to post

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import he from "he";
+import SearchInput from "./SearchInput";
 
 interface Post {
   id: string;
@@ -62,7 +63,7 @@ const SinglePost = ({
 
   return (
     <div className="w-full mx-auto 2xl:max-w-4xl flex flex-col justify-center relative p-8">
-      <nav aria-label="Breadcrumb" className="flex mb-5">
+      <nav aria-label="Breadcrumb" className="flex justify-between mb-5">
         <ol className="flex overflow-hidden rounded-lg border border-gray-200 text-gray-600">
           <li className="flex items-center">
             <a
@@ -77,20 +78,17 @@ const SinglePost = ({
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-
-              <span className="ms-1.5 text-xs font-medium"> Home </span>
+              <span className="ms-1.5 text-xs font-medium">Home</span>
             </a>
           </li>
-
           <li className="relative flex items-center">
             <span className="absolute inset-y-0 -start-px h-10 w-4 bg-gray-100 [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180"></span>
-
             <a
               href={`/${subreddit}`}
               className="flex h-10 items-center bg-white pe-4 ps-8 text-xs font-medium transition hover:text-gray-900"
@@ -99,6 +97,10 @@ const SinglePost = ({
             </a>
           </li>
         </ol>
+
+        <div className="flex justify-end">
+          <SearchInput />
+        </div>
       </nav>
 
       {posts.map((post) => (
