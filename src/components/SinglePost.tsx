8692ba16df3b5650268c4add1211e7198b7ebd0d
James Kua <kua.james30@gmail.com>
2024-05-08 10:45:18 +0800

    Update breadcrumb

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -75,13 +75,11 @@ const SinglePost = ({
 
   return (
     <div className="mx-auto md:w-8/12 xl:w-1/2 max-w-[90vw] flex flex-col justify-center relative py-4">
-      <nav aria-label="Breadcrumb" className="flex justify-between mb-5">
-        <ol className="flex overflow-hidden rounded-lg border border-gray-200 text-gray-600">
-          <li className="flex items-center">
-            <a
-              href="/"
-              className="flex h-8 items-center gap-1.5 bg-gray-100 px-4 transition hover:text-gray-900"
-            >
+      <nav aria-label="Breadcrumb" className="mb-5">
+        <ol className="flex items-center gap-1 text-sm text-gray-600">
+          <li>
+            <a href="/" className="block transition hover:text-gray-700">
+              <span className="sr-only">Home</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -90,20 +88,34 @@ const SinglePost = ({
                 stroke="currentColor"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              <span className="ms-1.5 text-xs font-medium">Home</span>
             </a>
           </li>
-          <li className="relative flex items-center">
-            <span className="absolute inset-y-0 -start-px h-8 w-4 bg-gray-100 [clip-path:_polygon(0_0,_0%_100%,_100%_50%)] rtl:rotate-180"></span>
+
+          <li className="rtl:rotate-180">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </li>
+
+          <li>
             <a
               href={`/${subreddit}`}
-              className="flex h-8 items-center bg-white pe-4 ps-8 text-xs font-medium transition hover:text-gray-900"
+              className="flex h-8 items-center bg-white text-gray-500 text-lg font-bold"
             >
               {subreddit}
             </a>
