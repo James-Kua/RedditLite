a21a0c6e448a3065407ce4ecb682493983dc123f
James Kua <kua.james30@gmail.com>
2024-06-30 17:38:25 +0800

    Fix post gallery image heights

--- src/components/PostGallery.tsx
+++ src/components/PostGallery.tsx
@@ -11,34 +11,39 @@ type PostGalleryProps = {
   mediaMetadata?: any[];
 };
 
-const settings = {
-  dots: true,
-  infinite: false,
-  speed: 200,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-};
-
 const PostGallery: React.FC<PostGalleryProps> = ({
   galleryData,
   mediaMetadata,
 }) => {
+  const maxImageWidth = window.innerWidth * 0.9;
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+
   return (
-    <div className="mb-8">
+    <div className="px-4">
       {galleryData ? (
         <Slider {...settings} className="mb-8">
           {galleryData.items.map((item) => (
-            <div
-              key={item.media_id}
-              className="h-full w-full flex items-center z-50"
-            >
-              <img
-                src={`https://i.redd.it/${item.media_id}.${parseImageType(
-                  mediaMetadata?.[item.media_id as unknown as number]?.m ?? ""
-                )}`}
-                className="relative rounded-lg overflow-hidden w-full h-auto block mt-2"
-                alt="Gallery Image"
-              />
+            <div key={item.media_id} className="flex justify-center">
+              <div
+                className="max-h-[90vh] w-full flex items-center justify-center"
+                style={{ maxWidth: `${maxImageWidth}px` }}
+              >
+                <img
+                  src={`https://i.redd.it/${item.media_id}.${parseImageType(
+                    mediaMetadata?.[item.media_id as unknown as number]?.m ?? ""
+                  )}`}
+                  className="max-h-[90vh] max-w-full object-contain rounded-lg"
+                  alt="Gallery Image"
+                />
+              </div>
             </div>
           ))}
         </Slider>
