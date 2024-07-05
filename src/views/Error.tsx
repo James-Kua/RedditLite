296a5a2dea0f753d7b1ebe3c833d5ac2616eab60
James Kua <kua.james30@gmail.com>
2024-07-05 08:45:04 +0800

    Add dark mode for error page

--- src/views/Error.tsx
+++ src/views/Error.tsx
@@ -1,11 +1,11 @@
 const Error = () => {
   return (
-    <div className="grid h-screen place-content-center bg-white px-4">
+    <div className="grid h-screen place-content-center dark:bg-custom-black bg-white px-4">
       <div className="text-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1024 768"
-          className="mx-auto h-56 w-auto text-black sm:h-64"
+          className="mx-auto h-56 w-auto text-black dark:text-gray-400 sm:h-64"
         >
           <g fill="none" fillRule="evenodd">
             <g fill="#FF5678">
@@ -21,11 +21,11 @@ const Error = () => {
           </g>
         </svg>
 
-        <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <h1 className="mt-6 text-2xl font-bold tracking-tight dark:text-white text-gray-900 sm:text-4xl">
           Uh-oh!
         </h1>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500 dark:text-gray-200">We can't find that page.</p>
       </div>
     </div>
   );
