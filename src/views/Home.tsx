664ee106d0d45d3ad5b020ecbf6d167caf18a71a
James Kua <kua.james30@gmail.com>
2024-07-15 00:08:00 +0800

    Add quick navigate to /r/popular in home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,26 @@
 import SearchInput from "../components/SearchInput";
 
 const Home = () => {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen px-4 gap-y-1 dark:bg-custom-black">
-        <h1 className="text-4xl font-bold font-mono tracking-wide text-gray-500 dark:text-white">RedditLite</h1>
-        <h4 className="tracking-widest text-gray-500 dark:text-white">Lightweight Reddit Browsing</h4>
-        <SearchInput />
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <div className="flex flex-col justify-center items-center h-screen px-4 gap-y-1 text-center dark:bg-custom-black">
+      <h1 className="text-4xl font-bold font-mono tracking-wide text-gray-500 dark:text-white">
+        RedditLite
+      </h1>
+      <h4 className="tracking-widest text-gray-500 dark:text-white">
+        Lightweight Reddit Browsing
+      </h4>
+      <SearchInput />
+      <p className="text-sm tracking-wide text-gray-500 dark:text-white mt-4">
+        Browse the most popular posts from{" "}
+        <a
+          href="/r/popular"
+          className="font-bold text-blue-500 dark:text-blue-300"
+        >
+          /r/popular
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Home;
