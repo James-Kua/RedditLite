6d6ce57f6791ace2ca5b1a6b87dbd66eae063ba4
James <61898455+James-Kua@users.noreply.github.com>
2024-07-19 20:37:52 +0800

    Support Parent Crosspost (#2)

---
+++ src/components/Post.tsx
@@ -0,0 +1,99 @@
+import he from "he";
+import type { Post } from "../types/post";
+import { isImage, parsePermalink } from "../utils/parser";
+import AuthorFlairText from "./AuthorFlairText";
+import LinkFlairText from "./LinkFlairText";
+import { FetchImage } from "../utils/image";
+import Thumbnail from "./Thumbnail";
+import SecureMediaEmbed from "./SecureMediaEmbed";
+import SecureMedia from "./SecureMedia";
+import SelfTextHtml from "./SelfTextHtml";
+import PostGallery from "./PostGallery";
+import PostStats from "./PostStats";
+import PostPreview from "./PostPreview";
+import CreatedEditedLabel from "./CreatedEditedLabel";
+import PostLock from "./PostLock";
+
+const PostComponent = ({ post }: { post: Post }) => {
+  return (
+    <div
+      key={post.id}
+      className="prose text-gray-500 prose-sm prose-headings:font-normal prose-headings:text-xl mx-auto w-full mb-8 overflow-auto"
+    >
+      <div className="flex items-center space-x-2">
+        <a href={`/user/${post.author}`}>
+          <h3 className="font-semibold text-blue-400">{post.author}</h3>
+        </a>
+        <AuthorFlairText
+          author_flair_richtext={post.author_flair_richtext}
+          author_flair_text={post.author_flair_text}
+          author_flair_background_color={post.author_flair_background_color}
+        />
+      </div>
+      <a href={`${post.crosspost_parent_list?.[0].permalink ?? post.url_overridden_by_dest ?? post.url}`}>
+        <CreatedEditedLabel created={post.created} edited={post.edited} />
+        <h2 className="text-lg my-2 font-semibold dark:text-white">
+          {he.decode(post.title)}
+        </h2>
+        <LinkFlairText
+          link_flair_richtext={post.link_flair_richtext}
+          link_flair_text={post.link_flair_text}
+          link_flair_background_color={post.link_flair_background_color}
+        />
+        {post.crosspost_parent_list ? (
+          <div className="px-4 pt-4 rounded-md border-2 mt-2 w-fit max-w-[90vw] inline-block">
+            <div className="font-bold dark:text-white mb-2">
+              {post.crosspost_parent_list[0].subreddit_name_prefixed}
+            </div>
+            <p className="text-gray-500 dark:text-slate-200">
+              <a
+                href={parsePermalink(post.crosspost_parent_list[0].permalink)}
+                className="text-blue-400"
+              >
+                <PostComponent post={post.crosspost_parent_list[0]} />
+              </a>
+            </p>
+          </div>
+        ) : post.secure_media_embed?.media_domain_url ? (
+          <SecureMediaEmbed
+            url_overridden_by_dest={post.url_overridden_by_dest}
+            playing={true}
+            {...post.secure_media_embed}
+          />
+        ) : post.secure_media ? (
+          <SecureMedia playing={true} {...post.secure_media} />
+        ) : post.media_metadata ? (
+          <div className="relative mt-2">
+            {post.gallery_data ? (
+              <PostGallery
+                galleryData={post.gallery_data}
+                mediaMetadata={post.media_metadata}
+              />
+            ) : null}
+          </div>
+        ) : post.preview ? (
+          <PostPreview preview={post.preview} />
+        ) : post.url_overridden_by_dest ? (
+          isImage(post.url_overridden_by_dest) ? (
+            <img
+              src={post.url_overridden_by_dest}
+              alt="url_overridden_by_dest"
+              className="relative rounded-md overflow-hidden xs:w-[184px] w-[284px] block mt-2 max-w-full"
+            />
+          ) : (
+            <FetchImage url={post.url_overridden_by_dest} />
+          )
+        ) : (
+          <Thumbnail thumbnail={post.thumbnail || ""} />
+        )}
+        {post.selftext_html && (
+          <SelfTextHtml selftext_html={post.selftext_html} />
+        )}
+      </a>
+      <PostLock locked={post.locked} />
+      <PostStats score={post.score} num_comments={post.num_comments} />
+    </div>
+  );
+};
+
+export default PostComponent;
