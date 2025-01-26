a549858c802a17723d8d5ff15b32e97b762769a6
James Kua <kua.james30@gmail.com>
2025-01-26 09:29:41 +0800

    Update fonts

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -12,7 +12,7 @@ const ExternalLink: React.FC<ExternalLinkProps> = ({
       href={url_overridden_by_dest}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-blue-400 dark:text-blue-300 bg-slate-100 dark:bg-zinc-800 mt-2 px-1 py-2 rounded-md whitespace-nowrap text-xs font-medium overflow-auto flex items-center tracking-wide"
+      className="text-blue-400 dark:text-blue-300 bg-slate-100 dark:bg-zinc-800 mt-2 px-1 py-2 rounded-md whitespace-nowrap text-xs font-medium overflow-auto flex items-center"
     >
       {url_overridden_by_dest}
       <ExternalLinkIcon />
