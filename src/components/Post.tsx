b8f3d6d2494063637a55d99a42a8f4bfc099f529
James Kua <kua.james30@gmail.com>
2025-06-28 21:43:14 +0800

    Improve post layout

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,17 +18,16 @@ import ExternalLink from "./ExternalLink";
 import UpvotePercentageLabel from "./UpvotePercentageLabel";
 
 const PostComponent = ({ post }: { post: Post }) => {
-
   document.title = post.title;
 
   return (
     <div
       key={post.id}
-      className="prose text-gray-500 prose-sm prose-headings:font-normal prose-headings:text-xl mx-auto w-full mb-4 overflow-auto"
+      className="bg-slate-200 dark:bg-neutral-800 shadow-md rounded-xl p-4 mb-4 w-full mx-auto prose prose-sm text-gray-700 dark:text-gray-300 prose-headings:font-semibold prose-headings:text-xl overflow-auto"
     >
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-2 mb-1">
         <a href={`/user/${post.author}`}>
-          <h3 className="font-semibold text-blue-400 whitespace-nowrap">{post.author}</h3>
+          <h3 className="text-blue-500 font-semibold whitespace-nowrap hover:underline">{post.author}</h3>
         </a>
         <AuthorFlairText
           author_flair_richtext={post.author_flair_richtext}
@@ -44,27 +43,26 @@ const PostComponent = ({ post }: { post: Post }) => {
         }`}
       >
         <CreatedEditedLabel created={post.created} edited={post.edited} />
-        <div>
+
+        <div className="mt-1">
           <UpvotePercentageLabel upvote_ratio={post.upvote_ratio} />
         </div>
-        <h2 className="text-lg my-2 font-semibold dark:text-white">
-          {he.decode(post.title)}
-        </h2>
+
+        <h2 className="text-lg font-bold dark:text-white my-2 group-hover:underline">{he.decode(post.title)}</h2>
+
         <LinkFlairText
           link_flair_richtext={post.link_flair_richtext}
           link_flair_text={post.link_flair_text}
           link_flair_background_color={post.link_flair_background_color}
         />
+
         {post.crosspost_parent_list ? (
-          <div className="px-4 pt-4 rounded-md border-2 mt-2 w-full inline-block">
+          <div className="px-4 pt-4 rounded-md border-2 border-gray-300 dark:border-gray-600 mt-3 bg-white dark:bg-neutral-900">
             <div className="font-bold dark:text-white mb-2">
               {post.crosspost_parent_list[0].subreddit_name_prefixed}
             </div>
-            <p className="text-gray-500 dark:text-slate-200">
-              <a
-                href={parsePermalink(post.crosspost_parent_list[0].permalink)}
-                className="text-blue-400"
-              >
+            <p className="text-gray-600 dark:text-slate-300">
+              <a href={parsePermalink(post.crosspost_parent_list[0].permalink)} className="text-blue-400">
                 <PostComponent post={post.crosspost_parent_list[0]} />
               </a>
             </p>
@@ -80,10 +78,7 @@ const PostComponent = ({ post }: { post: Post }) => {
         ) : post.media_metadata ? (
           <div className="relative mt-2">
             {post.gallery_data ? (
-              <PostGallery
-                galleryData={post.gallery_data}
-                mediaMetadata={post.media_metadata}
-              />
+              <PostGallery galleryData={post.gallery_data} mediaMetadata={post.media_metadata} />
             ) : null}
           </div>
         ) : post.preview ? (
@@ -93,7 +88,7 @@ const PostComponent = ({ post }: { post: Post }) => {
             <img
               src={post.url_overridden_by_dest}
               alt="url_overridden_by_dest"
-              className="mt-4 flex justify-center items-center max-w-full max-h-[500px] mx-auto border rounded-sm p-2 object-contain"
+              className="mt-4 max-w-full max-h-[500px] mx-auto border rounded-md p-2 object-contain"
             />
           ) : (
             <FetchImage url={post.url_overridden_by_dest} />
