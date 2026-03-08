f42c0df7d9a31f45fa57c7c810e8877e60720ae4
James Kua <kua.james30@gmail.com>
2026-03-08 14:02:35 +0800

    add site analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'  
+import betterlytics from "@betterlytics/tracker"
 
+betterlytics.init("reddit-lite-mmhc8b0s")
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
