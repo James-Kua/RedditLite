982f7c61333e0f4b8329bffaa1cd9a03fd1b26de
James <61898455+James-Kua@users.noreply.github.com>
2026-03-14 23:00:34 +0800

    Revamp to pinterest style feed (#101)
    * revamp to pinterest style feed
    
    * fix duplicate posts on load
    
    * add segmented time and sort option buttons

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -25,6 +25,7 @@ import ArrowIcon from "../static/ArrowIcon";
 import PollData from "./PollData";
 import { RedditApiClient } from "../api/RedditApiClient";
 import { useNavigate, useParams } from "react-router-dom";
+import SegmentedControl from "./SegmentedControl";
 
 export interface FeedProps {
   subreddit: string;
@@ -35,7 +36,7 @@ export interface FeedProps {
 const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort }) => {
   const params = useParams();
   const navigate = useNavigate();
-  
+
   const [posts, setPosts] = useState<Post[]>([]);
   const [subredditInfo, setSubredditInfo] = useState<Subreddit>();
   const [subredditRules, setSubredditRules] = useState<SubredditRules[]>([]);
@@ -46,7 +47,7 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
   const observer = useRef<IntersectionObserver | null>(null);
   const sentinel = useRef(null);
   const [isStarred, setIsStarred] = useState<boolean>(
-    localStorage.getItem("reddit-lite-starred")?.includes(subreddit) || false
+    localStorage.getItem("reddit-lite-starred")?.includes(subreddit) || false,
   );
 
   useEffect(() => {
@@ -60,15 +61,15 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
   const fetchPosts = useCallback(() => {
     if (!hasMore) return;
 
-    RedditApiClient.fetch(
-      `https://www.reddit.com/r/${subreddit}/${sort}.json?after=${after}&t=${time}&sr_detail=true`
-    )
+    RedditApiClient.fetch(`https://www.reddit.com/r/${subreddit}/${sort}.json?after=${after}&t=${time}&sr_detail=true`)
       .then((response) => response.json())
       .then((data) => {
-        const fetchedPosts = data.data.children.map(
-          (child: { data: Post }) => child.data
-        );
-        setPosts((prevPosts) => [...prevPosts, ...fetchedPosts]);
+        const fetchedPosts = data.data.children.map((child: { data: Post }) => child.data);
+        setPosts((prevPosts) => {
+          const existingPostIds = new Set(prevPosts.map((p: Post) => p.id));
+          const uniqueNewPosts = fetchedPosts.filter((p: Post) => !existingPostIds.has(p.id));
+          return [...prevPosts, ...uniqueNewPosts];
+        });
         setAfter(data.data.after);
         setHasMore(!!data.data.after);
       });
@@ -82,9 +83,7 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
     RedditApiClient.fetch(`https://www.reddit.com/r/${subreddit}/${sort}.json?t=${time}&sr_detail=true`)
       .then((response) => response.json())
       .then((data) => {
-        const fetchedPosts = data.data.children.map(
-          (child: { data: Post }) => child.data
-        );
+        const fetchedPosts = data.data.children.map((child: { data: Post }) => child.data);
         setPosts(fetchedPosts);
         setAfter(data.data.after);
         setHasMore(!!data.data.after);
@@ -126,9 +125,7 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
   }, [fetchPosts, hasMore]);
 
   const toggleSubredditStar = () => {
-    let starredSubreddits: string[] = JSON.parse(
-      localStorage.getItem("reddit-lite-starred") || "[]"
-    );
+    let starredSubreddits: string[] = JSON.parse(localStorage.getItem("reddit-lite-starred") || "[]");
 
     const subredditIndex = starredSubreddits.indexOf(subreddit);
     if (subredditIndex !== -1) {
@@ -137,10 +134,7 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
       starredSubreddits.push(subreddit);
     }
 
-    localStorage.setItem(
-      "reddit-lite-starred",
-      JSON.stringify(starredSubreddits)
-    );
+    localStorage.setItem("reddit-lite-starred", JSON.stringify(starredSubreddits));
 
     setIsStarred(!isStarred);
   };
@@ -152,11 +146,8 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
 
   return (
     <div className="dark:bg-custom-black dark:text-white">
-      <div className="md:w-8/12 xl:w-1/2 max-w-[95vw] mx-auto flex flex-col justify-center relative py-4">
-        <nav
-          aria-label="Breadcrumb"
-          className="flex items-center justify-between mb-5"
-        >
+      <div className="max-w-[95vw] mx-auto relative py-4">
+        <nav aria-label="Breadcrumb" className="flex items-center justify-between mb-5">
           <ol className="flex items-center gap-1 text-sm text-gray-600">
             <li>
               <a href="/" className="block transition hover:text-gray-700">
@@ -175,154 +166,129 @@ const Feed: React.FC<FeedProps> = memo(({ subreddit, initialTime, initialSort })
             >
               {isStarred ? "⭐" : "☆"}
             </button>
-            {
-              <SubredditIcon
-                community_icon={subredditInfo?.community_icon}
-                icon_img={subredditInfo?.icon_img}
-              />
-            }
-            <h1 className="text-gray-500 dark:text-white font-bold text-lg tracking-wide">
-              {subreddit}
-            </h1>
+            {<SubredditIcon community_icon={subredditInfo?.community_icon} icon_img={subredditInfo?.icon_img} />}
+            <h1 className="text-gray-500 dark:text-white font-bold text-lg tracking-wide">{subreddit}</h1>
           </ol>
           <div className="search-input">
             <SearchInput />
           </div>
         </nav>
-
         <div className="mb-2">
-          {subredditInfo && (
-            <SubredditInfo
-              subreddit={subredditInfo}
-              rules={subredditRules}
-            />
-          )}
+          {subredditInfo && <SubredditInfo subreddit={subredditInfo} rules={subredditRules} />}
         </div>
-
         <div className="mb-4 flex flex-wrap gap-3">
           {filterOptions.map((optionGroup, index) =>
             optionGroup.label === "Time" && sort !== "top" ? null : (
-              <div
-                className="flex items-center"
-                key={index}
-              >
-                <label className="mr-2 font-medium text-sm text-gray-700 dark:text-gray-300">
-                  {optionGroup.label}
-                </label>
-                <select
-                  value={optionGroup.label === "Sort by" ? sort : time}
-                  onChange={(e) => {
+              <div className="flex items-center overflow-x-auto hide-scrollbar" key={index}>
+                <label className="mr-2 font-medium text-sm text-gray-700 dark:text-gray-300">{optionGroup.label}</label>
+                <SegmentedControl
+                  options={optionGroup.options}
+                  currentValue={optionGroup.label === "Sort by" ? sort : time}
+                  onChange={(value) => {
                     switch (optionGroup.label) {
                       case "Sort by":
-                        setSort(e.target.value);
+                        setSort(value);
                         break;
                       case "Time":
-                        setTime(e.target.value);
+                        setTime(value);
                         break;
                     }
                   }}
-                  className="text-sm border border-gray-200 dark:border-gray-600 rounded-md px-1 py-1 font-medium text-gray-800 dark:text-gray-200 dark:bg-gray-800 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                >
-                  {optionGroup.options.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.key}
-                    </option>
-                  ))}
-                </select>
+                  label={optionGroup.label}
+                />
               </div>
-            )
+            ),
           )}
         </div>
-
-        {posts.map((post) => (
-          <div
-            key={post.id}
-            className="bg-slate-200 dark:bg-neutral-800 shadow-md rounded-xl p-2 mb-6 w-full mx-auto prose prose-sm text-gray-700 dark:text-gray-300 prose-headings:font-semibold prose-headings:text-xl"
-          >
-            <div>
-              <div className="flex items-center space-x-2 mb-2">
-                <img
-                  src={
-                    post.sr_detail?.community_icon?.length! > 1
-                      ? post.sr_detail?.community_icon.replace(/&amp;/g, "&")
-                      : post.sr_detail?.icon_img?.length! > 1
-                      ? post.sr_detail?.icon_img.replace(/&amp;/g, "&")
-                      : post.sr_detail?.header_img?.length! > 1
-                      ? post.sr_detail?.header_img.replace(/&amp;/g, "&")
-                      : "/fallback_reddit_icon.png"
-                  }
-                  alt={post.author}
-                  className="w-6 h-6 rounded-full bg-gray-300 dark:bg-gray-600"
-                />
-                <a href={`/user/${post.author}`}>
-                  <h3 className="text-blue-500 font-semibold whitespace-nowrap hover:underline">
-                    {post.author}
-                  </h3>
-                </a>
-                <AuthorFlairText
-                  author_flair_richtext={post.author_flair_richtext}
-                  author_flair_text={post.author_flair_text}
-                  author_flair_background_color={post.author_flair_background_color}
-                />
-              </div>
-              <CreatedEditedLabel created={post.created} edited={post.edited} />
-
-              <a href={parsePermalink(post.permalink)} className="block mt-2 group">
-                <h2 className="text-lg font-bold dark:text-white group-hover:underline">{he.decode(post.title)}</h2>
-                <LinkFlairText
-                  link_flair_richtext={post.link_flair_richtext}
-                  link_flair_text={post.link_flair_text}
-                  link_flair_background_color={post.link_flair_background_color}
-                />
-                <div
-                  className={`mt-3 ${
-                    post.thumbnail === "spoiler" || post.thumbnail === "nsfw" || post.over_18 ? "blur-sm" : ""
-                  }`}
-                >
-                  {post.secure_media_embed?.media_domain_url ? (
-                    <SecureMediaEmbed
-                      url_overridden_by_dest={post.url_overridden_by_dest}
-                      {...post.secure_media_embed}
-                    />
-                  ) : post.secure_media ? (
-                    <SecureMedia {...post.secure_media} />
-                  ) : post.media_metadata ? (
-                    <div className="relative mt-2">
-                      {post.gallery_data ? (
-                        <PostGallery galleryData={post.gallery_data} mediaMetadata={post.media_metadata} />
-                      ) : null}
-                    </div>
-                  ) : post.preview ? (
-                    <PostPreview preview={post.preview} />
-                  ) : post.url_overridden_by_dest ? (
-                    isImage(post.url_overridden_by_dest) ? (
-                      <img
-                        src={post.url_overridden_by_dest}
-                        alt="url_overridden_by_dest"
-                        className="mt-4 max-w-full max-h-[500px] mx-auto border rounded-md p-2 object-contain"
-                      />
-                    ) : (
-                      <FetchImage url={post.url_overridden_by_dest} />
-                    )
-                  ) : (
-                    <Thumbnail thumbnail={post.thumbnail || ""} />
-                  )}
-
-                  {post.poll_data && <PollData poll_data={post.poll_data} />}
-
-                  {post.selftext_html && <SelfTextHtml selftext_html={post.selftext_html} truncateLines={10} />}
-
-                  {post.url_overridden_by_dest && post.post_hint === "link" && (
-                    <ExternalLink url_overridden_by_dest={post.url_overridden_by_dest} />
-                  )}
-
-                  <PostStats score={post.score} num_comments={post.num_comments} />
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              className="bg-slate-200 dark:bg-neutral-800 shadow-md rounded-xl p-2 mb-4 w-full mx-auto prose prose-sm text-gray-700 dark:text-gray-300 prose-headings:font-semibold prose-headings:text-xl break-inside-avoid"
+            >
+              <div>
+                <div className="flex items-center space-x-2 mb-2">
+                  <img
+                    src={
+                      post.sr_detail?.community_icon?.length! > 1
+                        ? post.sr_detail?.community_icon.replace(/&amp;/g, "&")
+                        : post.sr_detail?.icon_img?.length! > 1
+                          ? post.sr_detail?.icon_img.replace(/&amp;/g, "&")
+                          : post.sr_detail?.header_img?.length! > 1
+                            ? post.sr_detail?.header_img.replace(/&amp;/g, "&")
+                            : "/fallback_reddit_icon.png"
+                    }
+                    alt={post.author}
+                    className="w-6 h-6 rounded-full bg-gray-300 dark:bg-gray-600"
+                  />
+                  <a href={`/user/${post.author}`}>
+                    <h3 className="text-blue-500 font-semibold whitespace-nowrap hover:underline">{post.author}</h3>
+                  </a>
+                  <AuthorFlairText
+                    author_flair_richtext={post.author_flair_richtext}
+                    author_flair_text={post.author_flair_text}
+                    author_flair_background_color={post.author_flair_background_color}
+                  />
                 </div>
-              </a>
+                <CreatedEditedLabel created={post.created} edited={post.edited} />
+
+                <a href={parsePermalink(post.permalink)} className="block mt-2 group">
+                  <h2 className="text-lg font-bold dark:text-white group-hover:underline">{he.decode(post.title)}</h2>
+                  <LinkFlairText
+                    link_flair_richtext={post.link_flair_richtext}
+                    link_flair_text={post.link_flair_text}
+                    link_flair_background_color={post.link_flair_background_color}
+                  />
+                  <div
+                    className={`mt-3 ${
+                      post.thumbnail === "spoiler" || post.thumbnail === "nsfw" || post.over_18 ? "blur-sm" : ""
+                    }`}
+                  >
+                    {post.secure_media_embed?.media_domain_url ? (
+                      <SecureMediaEmbed
+                        url_overridden_by_dest={post.url_overridden_by_dest}
+                        {...post.secure_media_embed}
+                      />
+                    ) : post.secure_media ? (
+                      <SecureMedia {...post.secure_media} />
+                    ) : post.media_metadata ? (
+                      <div className="relative mt-2">
+                        {post.gallery_data ? (
+                          <PostGallery galleryData={post.gallery_data} mediaMetadata={post.media_metadata} />
+                        ) : null}
+                      </div>
+                    ) : post.preview ? (
+                      <PostPreview preview={post.preview} />
+                    ) : post.url_overridden_by_dest ? (
+                      isImage(post.url_overridden_by_dest) ? (
+                        <img
+                          src={post.url_overridden_by_dest}
+                          alt="url_overridden_by_dest"
+                          className="mt-4 max-w-full max-h-125 mx-auto border rounded-md p-2 object-contain"
+                        />
+                      ) : (
+                        <FetchImage url={post.url_overridden_by_dest} />
+                      )
+                    ) : (
+                      <Thumbnail thumbnail={post.thumbnail || ""} />
+                    )}
+
+                    {post.poll_data && <PollData poll_data={post.poll_data} />}
+
+                    {post.selftext_html && <SelfTextHtml selftext_html={post.selftext_html} truncateLines={10} />}
+
+                    {post.url_overridden_by_dest && post.post_hint === "link" && (
+                      <ExternalLink url_overridden_by_dest={post.url_overridden_by_dest} />
+                    )}
+
+                    <PostStats score={post.score} num_comments={post.num_comments} />
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
-        <div ref={sentinel} className="h-1"></div>
+          ))}
+        </div>
+        <div ref={sentinel} className="h-1"></div>{" "}
       </div>
     </div>
   );
